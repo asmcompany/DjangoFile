123701eec78ba86f6107c6017c03533581c24cb1
asmcoco <mohamad.kz8846@gmail.com>
2020-08-08 02:43:21 -0700

    my change

--- products/models.py
+++ products/models.py
@@ -5,17 +5,20 @@ from .utils import unique_slug_generator
 from django.db.models.signals import pre_save
 from django.urls import reverse
 
+
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
     name, ext = os.path.splitext(base_name)
     return name, ext
 
+
 def upload_image_path(instance, filename):
-    new_name = random.randint(1,324345344332)
+    new_name = random.randint(1, 324345344332)
     name, ext = get_filename_ext(filename)
-  # final_name = f"{new_name}{ext}"
+    # final_name = f"{new_name}{ext}"
     final_name = f"{instance.id}-{instance.title}{ext}"
     return f"products/{final_name}"
+
 
 # Create your models here.
 
@@ -33,6 +36,7 @@ class ProductQuerySet(models.query.QuerySet):
     def active(self):
         return self.filter(active=True)
 
+
 class ProductManager(models.Manager):
 
     def all(self):
@@ -48,7 +52,7 @@ class ProductManager(models.Manager):
         return self.get_queryset().filter(featured=True)
 
     def get_by_id(self, productId):
-        qs =self.get_queryset().filter(id=productId)  # product.objects
+        qs = self.get_queryset().filter(id=productId)  # product.objects
         if qs.count() == 1:
             return qs.first()
         return None
@@ -65,17 +69,18 @@ class Product(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
-       # return f"/products/{self.slug}"
+        # return f"/products/{self.slug}"
         return reverse("products:detail", kwargs={"slug": self.slug})
+
     objects = ProductManager()
 
     def __str__(self):
         return self.title
 
+
 def product_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
-        
+
+
 pre_save.connect(product_pre_save_receiver, sender=Product)
-
-
